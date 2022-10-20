9de8b72010ed2e3d38ef30b14ab7224c2dfcd613
dc-ankou <94530449+dc-ankou@users.noreply.github.com>
2022-10-20 20:47:57 +0300

    Update card_locators.ts

--- src/resources/locators/card_locators.ts
+++ src/resources/locators/card_locators.ts
@@ -1,42 +1,82 @@
 export const cards_transforms: Transform[] = [
     new Transform({
-        position: new Vector3(32, 2, 32),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		11.102884292602539,
+		0.125,
+		11.298818588256836
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(33, 2, 32),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		68.56632995605469,
+		0.125
+		63.31983184814453
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(34, 2, 32),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		21.748592376708984,
+		0.125
+		28.769535064697266
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(31, 2, 32),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		20.951797485351562,
+		0.125
+		15.145642280578613
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(30, 2, 32),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		70.7455062866211,
+		0.125
+		73.62110137939453
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(32, 2, 31),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		68.09333801269531,
+		0.125
+		4.80036735534668
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(32, 2, 33),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		76.75358581542969,
+		0.125
+		12.089085578918457
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(32, 2, 34),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		39.0689697265625,
+		0.125
+		73.76509094238281
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(32, 2, 30),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		20.386241912841797,
+		0.125
+		53.08679962158203
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     }),
     new Transform({
-        position: new Vector3(31, 2, 31),
-        rotation: new Quaternion(0, 0, 0, 1)
+        position: new Vector3(
+		52.14436721801758,
+		0.125
+		16.046937942504883
+		),
+        rotation: Quaternion.Euler(90, 0, 0)
     })
 ]
