75c79edb76fed3272d6841b497aa17bc4209eb44
Vladislav Rytov <60110864+PartyMar@users.noreply.github.com>
2022-10-25 08:17:49 +0300

    return ui

--- src/game.ts
+++ src/game.ts
@@ -7,27 +7,134 @@ import { swapMansion } from "./modules/allowPlayerIn";
 import { openMainDoor } from "./modules/mansion";
 import { areaEntity } from "./modules/cameraMode";
 
-  
+import {
+    checkProgression,
+    progression,
+    updateProgression,
+    resetProgression,
+    userData, setUserData
+} from './halloweenQuests/progression'
+import { updateQuestUI } from './halloweenQuests/quest/questTasks'
+import { isEqual } from './halloweenQuests/utils/isEqual';
+
 addNPCs()
 addBoss()
 addCreeper()
-//openMainDoor()
+openMainDoor()
 scene.isSceneLoaded = true
 engine.addEntity(areaEntity)
+
+
+let updating = false
+function updateSceneUI() {
+    return executeTask(async () => {
+        let result = false
+        if (!updating) {
+            try {
+                updating = true
+                const curr_progression = await checkProgression()
+                log('checkProgression', curr_progression, progression)
+                if (curr_progression != null) {
+                    curr_progression.data['w1Found'] = true
+                    curr_progression.data['w2Found'] = true
+                    curr_progression.data['w3Found'] = true
+                    curr_progression.data['w4Found'] = true
+                    curr_progression.data['NPCIntroDay5'] = true
+
+                    if (progression.data != null && isEqual(progression.data, curr_progression.data)) {
+                        log('no changes')
+                        result = true
+                    } else {
+                        progression.data = curr_progression.data
+                        progression.day = curr_progression.day
+
+                        if (progression.data != null) {
+                            log('updateQuestUI', progression.day, progression.data)
+                            updateQuestUI(progression.data, progression.day)
+
+                            //updateSceneByProgression()
+                            result = true
+                        } else {
+                            log('progression problem', progression.data)
+                        }
+                    }
+                }
+            } catch (e) {
+                log('updating error', e)
+            }
+        } else {
+            log('last updating not complete')
+        }
+        log('updating end', updating, result)
+        updating = false
+        log('updating', updating, result)
+        if (result) {
+            progression.progressionChanged = false
+        }
+        return result
+    })
+}
+
+const updateProgressInterval = 1
+class UpdateSceneUISystem implements ISystem {
+    private timer = 0
+
+    constructor() { }
+
+    update(dt: number) {
+        this.timer += dt
+        if (this.timer > updateProgressInterval) {
+            this.timer = 0
+            if (progression.progressionChanged) {
+                log('progressionChanged', progression.progressionChanged)
+                const res = updateSceneUI()
+                log('updateSceneUI', res)
+            }
+        }
+    }
+}
+
+engine.addSystem(new UpdateSceneUISystem())
+
+
+onEnterSceneObservable.add((player) => {
+    log('player entered scene: ', player.userId)
+    executeTask(async () => {
+        if (!userData) {
+            await setUserData()
+        }
+
+        if (userData.userId == player.userId) {
+            updateSceneUI()
+        }
+    })
+})
+
+onSceneReadyObservable.add(() => {
+    log('onSceneReadyObservable')
+    executeTask(async () => {
+        //await setUpScene()
+        updateSceneUI()
+    })
+})
+
+
+
 
 //mouse input
 const input = Input.instance
 input.subscribe("BUTTON_DOWN", ActionButton.POINTER, true, e => {
- // log("pointer POINTER Down", e)  
-  setGunActive()  
-  
+    // log("pointer POINTER Down", e)  
+    setGunActive()
+
 })
 
 input.subscribe("BUTTON_UP", ActionButton.POINTER, false, e => {
-  //log("pointer POINTER UP", e)  
-  setGunInActive()
+    //log("pointer POINTER UP", e)  
+    setGunInActive()
 })
 
 onSceneReadyObservable.add(() => {
-  swapMansion('out')
+    swapMansion('out')
 })
+
